b704e8f7060b828692fe0800aa584d0ce41577b2
Georgia Raynes-Greenow <geray1@student.monash.edu>
2017-05-17 20:14:44 +1000

    update

--- project3ideas/assets/js/main.js
+++ project3ideas/assets/js/main.js
@@ -1,5 +1,62 @@
 $(document).ready(function() {
 
+    $('.ios67').mouseenter(function() {
+      $('.ios67__image').addClass('showme');
+    });
+
+    $('.ios67').mouseleave(function() {
+      $('.ios67__image').removeClass('showme');
+    });
+
+
+
+
+
+
+    $('.NewYorkReviewofBooks').mouseenter(function() {
+      $('.cover__image').addClass('showme');
+    });
+
+    $('.NewYorkReviewofBooks').mouseleave(function() {
+      $('.cover__image').removeClass('showme');
+    });
+
+
+
+    $('.NewYorkReviewofBooks').mouseenter(function() {
+      $('.inside__image').addClass('showme');
+    });
+
+    $('.NewYorkReviewofBooks').mouseleave(function() {
+      $('.inside__image').removeClass('showme');
+    });
+
+
+
+
+
+
+
+    $('.demonstration').mouseenter(function() {
+      $('.mosesark__image').addClass('showme');
+    });
+
+    $('.demonstration').mouseleave(function() {
+      $('.mosesark__image').removeClass('showme');
+    });
+
+
+    $('.seefigure1').mouseenter(function() {
+      $('.graph__image').addClass('showme');
+    });
+
+    $('.seefigure1').mouseleave(function() {
+      $('.graph__image').removeClass('showme');
+    });
+
+
+
+
 
 
 
