2d2724513b7c599fd1d43e3a1d1b7efd5b12a61f
Georgia Raynes-Greenow <geray1@student.monash.edu>
2017-05-18 22:48:43 +1000

    update

--- project3ideas/assets/js/main.js
+++ project3ideas/assets/js/main.js
@@ -10,6 +10,110 @@ $(document).ready(function() {
 
 
 
+    $('.announcement').mouseenter(function() {
+      $('.disfluenc__image').addClass('showme');
+    });
+
+    $('.announcement').mouseleave(function() {
+      $('.disfluenc__image').removeClass('showme');
+    });
+
+
+
+
+
+
+
+
+    $('.noah').mouseenter(function() {
+      $('.noah__image').addClass('showme');
+    });
+
+    $('.noah').mouseleave(function() {
+      $('.noah__image').removeClass('showme');
+    });
+
+
+
+
+    $('.moses').mouseenter(function() {
+      $('.moses__image').addClass('showme');
+    });
+
+    $('.moses').mouseleave(function() {
+      $('.moses__image').removeClass('showme');
+    });
+
+
+
+
+    $('.theark').mouseenter(function() {
+      $('.noahsark').addClass('showme');
+    });
+
+    $('.theark').mouseleave(function() {
+      $('.noahsark').removeClass('showme');
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+    $('.demonstration').mouseenter(function() {
+      $('.mosesark__image').addClass('showme');
+    });
+
+    $('.demonstration').mouseleave(function() {
+      $('.mosesark__image').removeClass('showme');
+    });
+
+
+
+
+
+    $('.screensfrom1985').mouseenter(function() {
+      $('.apple1985__image').addClass('showme');
+    });
+
+    $('.screensfrom1985').mouseleave(function() {
+      $('.apple1985__image').removeClass('showme');
+    });
+
+
+    $('.todaysscreens').mouseenter(function() {
+      $('.apple2017__image').addClass('showme');
+    });
+
+    $('.todaysscreens').mouseleave(function() {
+      $('.apple2017__image').removeClass('showme');
+    });
+
+
+
+
+
+
+
+    $('.seefigure1').mouseenter(function() {
+      $('.graphs__image').addClass('showme');
+    });
+
+    $('.seefigure1').mouseleave(function() {
+      $('.graphs__image').removeClass('showme');
+    });
+
+
+
+
+
 
 
 
@@ -30,31 +134,6 @@ $(document).ready(function() {
     $('.NewYorkReviewofBooks').mouseleave(function() {
       $('.inside__image').removeClass('showme');
     });
-
-
-
-
-
-
-
-    $('.demonstration').mouseenter(function() {
-      $('.mosesark__image').addClass('showme');
-    });
-
-    $('.demonstration').mouseleave(function() {
-      $('.mosesark__image').removeClass('showme');
-    });
-
-
-    $('.seefigure1').mouseenter(function() {
-      $('.graph__image').addClass('showme');
-    });
-
-    $('.seefigure1').mouseleave(function() {
-      $('.graph__image').removeClass('showme');
-    });
-
-
 
 
 
